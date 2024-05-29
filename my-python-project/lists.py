a7b7e1d8eb0430febe35f7700a680f5fa50fcfb1
Faddah Wolf <my_biz@me.com>
2024-05-29 12:56:04 -0700

    Added lists.py to show working with python lists.

---
+++ my-python-project/lists.py
@@ -0,0 +1,9 @@
+my_list = ["January", "February", "March"]
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+my_list.append("April")
+print("and now it's time for the breakdown...")
+for month in my_list:
+    print(month)
+print(my_list[4])
