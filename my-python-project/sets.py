03fbcaed69f75a1ade5ab9dc43057b36fb1d14eb
Faddah Wolf <my_biz@me.com>
2024-06-11 12:15:51 -0700

    Examples of Python sets from Nana's course.

--- my-python-project/sets.py
+++ my-python-project/sets.py
@@ -1,6 +1,32 @@
+from hmac import new
+
+
 my_set = {"January", "February", "March"}
 for element in my_set:
 	print(element)
   
 my_set.add('April')
-print(my_set)
+for element in my_set:
+	print(element)
+
+
+my_set.add('March')
+for element in my_set:
+	print(element)
+my_set.remove('January')
+print(element)
+
+my_list = ["January", "February", "March", "January"]
+print(my_list)
+my_list.remove("January")
+print(my_list)
+
+new_set = set(my_list)
+print(new_set)
+new_set.add("April")
+print(new_set)
+new_set.remove("January")
+print(new_set)
+
+
+ 
