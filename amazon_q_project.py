c81ec7f7d5bb583fa02fabcc01c6705b7081e64f
Faddah Wolf <my_biz@me.com>
2025-02-11 11:05:05 -0800

    Added an Amazon Q created Python example.

---
+++ amazon_q_project.py
@@ -0,0 +1,13 @@
+import random
+
+# Create a list of 10 numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Generate a random number between 0 and 9
+random_number = random.randint(0, 9)
+
+# Get the number at the random index
+suggestion = numbers[random_number]
+
+# Display the suggestion
+print(suggestion)
